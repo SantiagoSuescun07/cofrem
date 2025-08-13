7ec0d9aea147e53f32966720d7d7afd1ebf64cdc
Santiago Suescun <santiago@192.168.1.6>
2025-08-13 02:56:11 -0500

    fix: origin google .env

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
     experimental: {
       serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
     },
-    env: {
-      NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
-    },
   };
 
 export default nextConfig;
