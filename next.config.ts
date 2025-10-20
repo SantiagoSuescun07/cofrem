594f4b6d823183d04f095723b4b783b91558d44a
Santiago Suecun <nspes2020@gmail.com>
2025-10-20 14:55:41 -0500

    new directory dynamic

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "backoffice.cofrem.com.co",
-        pathname: "/**", // permite cualquier ruta
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
       },
     ],
   },
+
+  // ⬇️ Antes estaba en experimental.serverComponentsExternalPackages
+  //    Ahora va a nivel raíz como serverExternalPackages
+  serverExternalPackages: ["@prisma/client", "bcryptjs"],
+
   experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
+    // deja aquí solo otras flags experimentales válidas si las usas
   },
 };
 
